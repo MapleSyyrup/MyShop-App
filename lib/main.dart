76d9c72c9302a9a1010f863560edb1cf15a28f5b
MapleSyyrup <cha.cha.bumanlag@gmail.com>
2020-12-27 13:50:27 +0800

    186. Working on the Products Grid Item and Widgets
    - Use GridView.builder for the product overview screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,24 +9,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a shop'),
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ProductsOverviewScreen(),
     );
   }
 }
