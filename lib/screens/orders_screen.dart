921d36e17e9d25aa03623b1cade3ab10dc86f3f8
MapleSyyrup <cha.cha.bumanlag@gmail.com>
2021-01-03 23:19:57 +0800

    208 - 212. Adding an Orders Screen
    - Add an order screen where the user can see all the orders
    - Add a side drawer

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/app_drawer.dart';
+import '../widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+    final orderDt = orderData.orders;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      drawer: AppDrawer(),
+      body: ListView.builder(
+        itemCount: orderDt.length,
+        itemBuilder: (ctx, i) => OrderItem(
+          order: orderDt[i],
+        ),
+      ),
+    );
+  }
+}
