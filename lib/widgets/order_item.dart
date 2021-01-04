977e0c279377440ad89dd38e01fb55377f217e05
MapleSyyrup <cha.cha.bumanlag@gmail.com>
2021-01-04 08:25:16 +0800

    fix: PR Feedback
    - Convert some functions into expression
    - Implement ExpansionTile widget

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+// import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -14,60 +14,35 @@ class OrderItem extends StatefulWidget {
 }
 
 class _OrderItemState extends State<OrderItem> {
-  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget.order.amount}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
-            ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(
-                  () {
-                    _expanded = !_expanded;
-                  },
-                );
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 10, 120),
-              child: ListView(
-                children: widget.order.products
-                    .map((prod) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              prod.title,
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              '${prod.quantity}x \$${prod.price}',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
-                        ))
-                    .toList(),
-              ),
-            )
-        ],
+    return ExpansionTile(
+      title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+      subtitle: Text(
+        DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
       ),
+      children: widget.order.products
+          .map((prod) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    prod.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '${prod.quantity}x \$${prod.price}',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey,
+                    ),
+                  )
+                ],
+              ))
+          .toList(),
     );
   }
 }
