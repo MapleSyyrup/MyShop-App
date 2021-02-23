ae700baf9273cb5ec2c4b708742dd42c59a26038
MapleSyyrup <cha.cha.bumanlag@gmail.com>
2021-02-23 17:51:29 +0800

    Deleting products in database
    - Delete products permanently in database
    - If there is a problem in deleting an item, it throws an exception that
      the product cannot be deleted.

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import '../models/constants.dart';
+import '../models/http_exception.dart';
 import 'product.dart';
 
 ///List of products
@@ -94,9 +95,19 @@ class ProductsProvider with ChangeNotifier {
     }
   }
 
-  ///Deletes a product in the list
-  void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+  ///Deletes a product in the list and in the database
+  Future<void> deleteProduct(String id) async {
+    final url = 'https://shop-app-4ed38-default-rtdb.firebaseio.com/products/$id.json';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Could not delete product.');
+    }
+    existingProduct = null;
   }
 }
