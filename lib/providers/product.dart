c2ae09da7696406b7688ffe4625625b9a2b7f86c
MapleSyyrup <cha.cha.bumanlag@gmail.com>
2021-02-25 17:44:04 +0800

    Updating the Favorite status Optimistically
    - Add the favorite status in firebase

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:myshop_app/models/constants.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,9 +21,27 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
   ///If the favorite button is pressed, the item will be added as favorite
-  void toggleFavoriteStatus() {
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = '${Constants.url}/products/$id.json';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({'isFavorite': isFavorite}),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
